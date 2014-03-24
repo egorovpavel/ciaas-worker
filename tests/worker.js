fe8ece5b978df3f1c1b60e8ddc1fec95ca020cb7
Pavel Egorov <pavel@egorov.ru>
2014-03-24 22:14:23 +0000

    Execution of item is now timeout aware

--- tests/worker.js
+++ tests/worker.js
@@ -3,18 +3,20 @@ var Worker = require('../lib/worker.js');
 describe('Worker', function() {
 
 	it('should execute working item inside container and exit with status code 0', function(done, fail) {
+		this.timeout(5000);
 		var worker = new Worker();
 		var item = {
 			config: {
 				language: "JS",
+				timeout: 5000
 			},
 			payload: {
 				commands: [
-					"echo 'Hello world'"
+					"echo 'Hello world'",
 				]
 			}
 		};
-		
+
 		worker.put(item).on('complete', function(data) {
 			data.StatusCode.should.be.equal(0);
 			done();
@@ -22,14 +24,37 @@ describe('Worker', function() {
 	});
 
 	it('should execute working item inside container and exit with status code different than 0', function(done, fail) {
+		this.timeout(5000);
 		var worker = new Worker();
 		var item = {
 			config: {
 				language: "JS",
+				timeout: 5000
 			},
 			payload: {
 				commands: [
 					"this must fail"
+				]
+			}
+		};
+
+		worker.put(item).on('complete', function(data) {
+			data.StatusCode.should.be.not.equal(0);
+			done();
+		});
+	});
+
+	it('should execute working item inside container and exit with status code 100 by timeout', function(done, fail) {
+		this.timeout(5000);
+		var worker = new Worker();
+		var item = {
+			config: {
+				language: "JS",
+				timeout: 1000
+			},
+			payload: {
+				commands: [
+					"sleep 2"
 				]
 			}
 		};
