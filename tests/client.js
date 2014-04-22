f16fc463048962f26a8574bc94b5e604f35ca400
Pavel Egorov <pavel@egorov.ru>
2014-04-22 23:41:44 +0100

    use strict added

--- tests/client.js
+++ tests/client.js
@@ -3,56 +3,79 @@ var should = require('should');
 var Client = require('../lib/client.js');
 var dnode = require('dnode');
 
-describe('client', function() {
-	it('should connect toserver and inform health', function(done, fail) {
-		this.timeout(5000);
+describe('Client', function() {
+	describe('Connection', function() {
+		it('Should be established', function(done, fail) {
+			this.timeout(5000);
 
-		var client = new Client(3000);
-		var server = dnode(function(remote, connection) {
-			this.join = function(id, callback) {
-				callback(id);
-				remote.health(function(result) {
+			var client = new Client(3000);
+			var server = dnode(function(remote, connection) {
+				this.join = function(id, callback) {
+
+				}
+				connection.on('ready', function() {
 					client.stop();
 					server.close();
 					done();
 				});
-			}
-		}).listen(3000);
+			}).listen(3000);
 
-		client.start();
+			client.start();
 
+		});
 	});
-	it('should connect toserver and run job item', function(done, fail) {
-		this.timeout(5000);
 
-		var item = {
-			config: {
-				language: "JS",
-				timeout: 5000
-			},
-			payload: {
-				commands: [
-					"echo 'Hello world'",
-				]
-			}
-		};
+	describe('After established connection', function() {
+		it('Should perform hand shake with pool', function(done, fail) {
+			this.timeout(5000);
 
-		var server = dnode(function(remote, connection) {
-			this.join = function(id, callback) {
-				callback(id);
+			var client = new Client(3000);
+			var server = dnode(function(remote, connection) {
+				this.join = function(id, callback) {
+					callback(id);
+					remote.health(function(result) {
+						client.stop();
+						server.close();
+						done();
+					});
+				}
+			}).listen(3000);
 
-				remote.exec(item, function(result) {
-					client.stop();
-					server.close();
-					result.StatusCode.should.be.equal(0);
-					done();
-				});
-			}
-		}).listen(3000);
+			client.start();
 
-		var client = new Client(3000);
+		});
+		it('Should connect toserver and run job item', function(done, fail) {
+			this.timeout(5000);
 
-		client.start();
+			var item = {
+				config: {
+					language: "JS",
+					timeout: 5000
+				},
+				payload: {
+					commands: [
+						"echo 'Hello world'",
+					]
+				}
+			};
+			var client = new Client(3000);
+			var server = dnode(function(remote, connection) {
+				this.join = function(id, callback) {
+					callback(id);
 
+					remote.exec(item, function(result) {
+						client.stop();
+						server.close();
+						result.StatusCode.should.be.equal(0);
+						done();
+					});
+				}
+			}).listen(3000);
+
+			client.start();
+
+		});
 	});
+
+
 });
