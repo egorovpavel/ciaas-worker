bec64aece5fe655c89dcf641d17becdfadf081ec
Pavel Egorov <pavel@egorov.ru>
2014-05-01 15:51:40 +0100

    Simple shutdown sequence implemented

--- tests/client.js
+++ tests/client.js
@@ -4,10 +4,15 @@ var Client = require('../lib/client.js');
 var dnode = require('dnode');
 var logger = require('winston');
 
-describe('Client', function() {
-	describe('Connection', function() {
-		it('Should be established', function(done, fail) {
-			this.timeout(5000);
+logger.setLevels({debug: 0, info: 1, silly: 2, warn: 3, error: 4});
+logger.addColors({debug: 'green', info: 'cyan', silly: 'magenta', warn: 'yellow', error: 'red'});
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, { level: 'debug', colorize: true });
+
+describe('Client', function () {
+    describe('Connection', function () {
+        it('Should be established', function (done, fail) {
+            this.timeout(5000);
 
             var client = new Client('127.0.0.1', 3000, logger);
 
@@ -19,9 +24,9 @@ describe('Client', function() {
                 };
             }).listen(3000);
 
-			client.start();
+            client.start();
 
-		});
+        });
         it('Emits "disconnect" event on failure', function (done, fail) {
             this.timeout(5000);
 
@@ -32,57 +37,105 @@ describe('Client', function() {
             client.start();
 
         });
-	});
+    });
 
-	describe('After established connection', function() {
-		it('Should perform hand shake with pool', function(done, fail) {
-			this.timeout(5000);
+    describe('After established connection', function () {
+        it('Should perform hand shake with pool', function (done, fail) {
+            this.timeout(5000);
 
             var client = new Client('127.0.0.1', 3000, logger);
-			var server = dnode(function(remote, connection) {
-				this.join = function(id, callback) {
-					callback(id);
-					remote.health(function(result) {
-						client.stop();
-						server.close();
-						done();
-					});
-				}
-			}).listen(3000);
+            var server = dnode(function (remote, connection) {
+                this.join = function (id, callback) {
+                    callback(id);
+                    remote.health(function (result) {
+                        client.stop();
+                        server.close();
+                        done();
+                    });
+                }
+            }).listen(3000);
 
-			client.start();
+            client.start();
 
-		});
-		it('Should connect toserver and run job item', function(done, fail) {
-			this.timeout(5000);
+        });
+        it('Should connect toserver and run job item', function (done, fail) {
+            this.timeout(5000);
 
-			var item = {
-				config: {
-					language: "JS",
-					timeout: 5000
-				},
-				payload: {
-					commands: [
+            var item = {
+                config: {
+                    language: "JS",
+                    timeout: 5000
+                },
+                payload: {
+                    commands: [
                         "echo 'Hello world'"
-					]
-				}
-			};
+                    ]
+                }
+            };
             var client = new Client('127.0.0.1', 3000, logger);
-			var server = dnode(function(remote, connection) {
-				this.join = function(id, callback) {
-					callback(id);
+            var server = dnode(function (remote, connection) {
+                this.join = function (id, callback) {
+                    callback(id);
 
-					remote.exec(item, function(result) {
-						client.stop();
-						server.close();
-						result.StatusCode.should.be.equal(0);
-						done();
-					});
-				}
-			}).listen(3000);
+                    remote.submit(1);
+                };
+                this.accept = function (itemId, callback) {
+                    itemId.should.be.equal(1);
+                    // get item by id
+                    remote.exec(item, function (result) {
+                        client.stop();
+                        server.close();
+                        result.StatusCode.should.be.equal(0);
+                        done();
+                    });
+                };
+                this.reject = function (itemId, callback) {
+                    fail();
+                };
+            }).listen(3000);
 
-			client.start();
+            client.start();
 
-		});
-	});
+        });
+        it('Should not accept job in shutdown state', function (done, fail) {
+            this.timeout(5000);
+
+            var item = {
+                config: {
+                    language: "JS",
+                    timeout: 5000
+                },
+                payload: {
+                    commands: [
+                        "sleep 3"
+                    ]
+                }
+            };
+            var client = new Client('127.0.0.1', 3000, logger);
+            var server = dnode(function (remote, connection) {
+                this.join = function (id, callback) {
+                    callback(id);
+                    remote.submit(1);
+                    remote.shutdown();
+                    remote.submit(1);
+                };
+                this.accept = function (itemId, callback) {
+                    remote.exec(item, function (result) {
+                    });
+                };
+                this.reject = function (id, callback) {
+                    callback();
+                    client.stop();
+                    server.close();
+                    done();
+                };
+                this.leave = function (id, callback) {
+
+                }
+            }).listen(3000);
+
+            client.start();
+
+        });
+    });
 });
