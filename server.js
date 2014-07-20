f3b6ae2f77b69e38ae8c792c57d0c7c6e7653b3f
Egorov Pavel <egorovpasha@gmail.com>
2014-07-20 22:04:00 +0000

    added PID file handling for monitoring purposes

---
+++ server.js
@@ -0,0 +1,22 @@
+'use strict';
+var Client = require('./lib/client.js');
+var Logger = require('winston');
+var config = require('./config.json')[process.env.NODE_ENV || 'development'];
+var npid = require('npid');
+
+try {
+
+    var pid = npid.create('/var/run/ci_worker.pid');
+    var controlledExit = function(){
+    	pid.removeOnExit();
+    	process.exit(1);
+    };
+    var client = new Client(config.host, config.port, Logger);
+    process.on('SIGINT',controlledExit).on('SIGTERM', controlledExit);
+    pid.removeOnExit();
+} catch (err) {
+    console.log(err);
+    process.exit(1);
+}
+
+Logger.info("STARTED");
--- service.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-var Client = require('./lib/client.js');
-var Logger = require('winston');
-var config = require('./config.json')[process.env.NODE_ENV || 'development'];
-var client = new Client(config.host, config.port, Logger);
-
-Logger.info("STARTED");
